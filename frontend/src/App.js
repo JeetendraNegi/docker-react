19f27f620c0fb1d7176fbc30253010f1b44d4054
Jeetendra Negi <40231981+JeetendraNegi@users.noreply.github.com>
2024-01-25 19:18:48 +0530

    Update App.js
    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There.
+          Hi There!.
         </p>
         <a
           className="App-link"
